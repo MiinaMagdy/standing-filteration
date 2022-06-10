015c53a388ee4f975382ddcca33c075dcce6625b
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-06-10 20:52:47 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -10,10 +10,15 @@ window.onload=function(){
         if (result.box) {
             // make #txt is readonly
             document.querySelector('#txt').setAttribute('readonly', 'readonly');
+            document.querySelector('#txt').style.backgroundColor = '#f5f5f5';
+            document.querySelector('#txt').style.border = '1px solid #ccc';
         }
         else {
             // make #txt is not readonly
             document.querySelector('#txt').removeAttribute('readonly');
+
+            document.querySelector('#txt').style.backgroundColor = '';
+            document.querySelector('#txt').style.border = '';
         }
     });
     
@@ -35,14 +40,17 @@ window.onload=function(){
         if (e.target.checked) {
             // make #txt is readonly
             document.querySelector('#txt').setAttribute('readonly', 'readonly');
+            document.querySelector('#txt').style.backgroundColor = '#f5f5f5';
+            document.querySelector('#txt').style.border = '1px solid #ccc';
         }
         else {
             // make #txt is not readonly
             document.querySelector('#txt').removeAttribute('readonly');
+            document.querySelector('#txt').style.backgroundColor = '';
+            document.querySelector('#txt').style.border = '';
         }
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {currTab: tabs[0], condition: e.target.checked, txt: document.querySelector('#txt').value});
         });
     });
-
 }
